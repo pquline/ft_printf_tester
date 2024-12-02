0f4f29cf699ce25e26d5255533510b5469f8b12d
Pauline Fischof <pfischof@student.42.fr>
2024-12-02 21:20:32 +0100

    chore: add -q (quiet) flag to valgrind

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ FT_PRINT_TESTER_C="ft_printf_tester.c"
 cd ..
 make re
 cc -Wall -Wextra -Werror -g3 -I $INCLUDES $FT_PRINTF_TESTER_DIR/$FT_PRINT_TESTER_C -L. -lftprintf
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all --track-fds=yes --trace-children=yes ./a.out
+valgrind -q --leak-check=full --track-origins=yes --show-leak-kinds=all --track-fds=yes --trace-children=yes ./a.out
 rm ./a.out
 make fclean
 cd $FT_PRINTF_TESTER_DIR
